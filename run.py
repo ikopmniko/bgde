ee6174de81134e1c6c458a99ba812784bf5baa25
ikopmniko <hannapitman786@gmail.com>
2025-05-29 16:18:23 +0700

    Update run.py

--- run.py
+++ run.py
@@ -10,12 +10,12 @@ SUPABASE_TABLE_NAME = "scp"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # API Serper
-API_KEY = "c118da989b06f18312b4f16a42bbe9a8f448c750"
+API_KEY = "2ce24b7ee5ee7b0cf9691e12f2c695af9b2a4fd5"
 URL_API = "https://google.serper.dev/search"
 
 # Rentang baris yang akan diproses
-mulai = 85000
-endnya = 87500
+mulai = 87500
+endnya = 90000
 
 headers = {
     "X-API-KEY": API_KEY,
